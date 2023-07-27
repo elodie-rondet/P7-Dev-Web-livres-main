0f3331de1123fb48d875544eaf88746e4c3e2893
Elodie RONDET <49192803+melisandria@users.noreply.github.com>
2023-07-27 16:35:40 +0200

    Update storage.js

--- backend/middleware/storage.js
+++ backend/middleware/storage.js
@@ -1,22 +1,33 @@
 const multer = require('multer');
 const { Book } = require("../models/Book");
 
+
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
-  'image/png': 'png'
+  'image/png': 'png',
+  'image/webp': 'webp'
 };
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
 	const id = req.params.id;
 	callback(null, "image");
+  callback(null, "image/modifier");
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    const fileSize = parseInt(req.headers["content-length"]);
+    if (fileSize > 4194304) 
+      callback(new Error("La taille du fichier est supérieure à la limite autorisée"), false);
+   else if (file.mimetype !== "image/png" && file.mimetype !== "image/jpg" && file.mimetype !== "image/jpeg")
+    callback(new Error("Veuillez sélectionner un fichier dont l'extension est autorisée"), false);
+    else
+      callback(null, name + Date.now() + '.' + extension);
+
   }
+
 });
 
 module.exports = { storage };
